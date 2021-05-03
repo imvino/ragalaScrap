247363efcb16e548e1a05adba64f06d047c83c56
imvino <49083863+imvino@users.noreply.github.com>
2021-05-03 18:47:05 +0530

    web directory

---
+++ Web/functions.js
@@ -0,0 +1,57 @@
+const {chromium} = require('playwright');
+
+(async () => { //, slowMo: 50
+    var start = new Date()
+    var hrstart = process.hrtime()
+    let data = [];
+    let list = [];
+    let delcount = 26;
+    let counter = 0;
+    //devtools: true
+    const browser = await chromium.launch({headless: false,});
+
+    let webpages = ['https://www.ragalahari.com/functions/a/search.aspx', 'https://www.ragalahari.com/functions/b/search.aspx', 'https://www.ragalahari.com/functions/c/search.aspx', 'https://www.ragalahari.com/functions/d/search.aspx', 'https://www.ragalahari.com/functions/e/search.aspx', 'https://www.ragalahari.com/functions/f/search.aspx', 'https://www.ragalahari.com/functions/g/search.aspx', 'https://www.ragalahari.com/functions/h/search.aspx', 'https://www.ragalahari.com/functions/i/search.aspx', 'https://www.ragalahari.com/functions/j/search.aspx', 'https://www.ragalahari.com/functions/k/search.aspx', 'https://www.ragalahari.com/functions/l/search.aspx', 'https://www.ragalahari.com/functions/m/search.aspx', 'https://www.ragalahari.com/functions/n/search.aspx', 'https://www.ragalahari.com/functions/o/search.aspx', 'https://www.ragalahari.com/functions/p/search.aspx', 'https://www.ragalahari.com/functions/q/search.aspx', 'https://www.ragalahari.com/functions/r/search.aspx', 'https://www.ragalahari.com/functions/s/search.aspx', 'https://www.ragalahari.com/functions/t/search.aspx', 'https://www.ragalahari.com/functions/u/search.aspx', 'https://www.ragalahari.com/functions/v/search.aspx', 'https://www.ragalahari.com/functions/w/search.aspx', 'https://www.ragalahari.com/functions/x/search.aspx', 'https://www.ragalahari.com/functions/y/search.aspx', 'https://www.ragalahari.com/functions/z/search.aspx'];
+
+    const context = await browser.newContext();
+
+    async function run(webpage) {
+        try{
+            const page = await context.newPage();
+            // await page.route('**/*.{png,jpg,jpeg,html,js,json,svg,css,woff,woff2,ico}', route => {
+            //     route.abort()
+            // });
+            await page.goto(webpage)
+            const ele_gal = await page.$$("a.galleryname");
+            Array(ele_gal.length).fill(0).map((v, i) => {
+                list[i] = ele_gal[i].innerText();
+            });
+            data['list'] = await Promise.all(list)
+            // console.log(data);
+            await page.close();
+            console.log('list => ' + webpage)
+            counter++
+            var end = new Date() - start,
+                hrend = process.hrtime(hrstart)
+            console.info('Execution time: %dms', end)
+            console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
+            console.log(counter)
+            if (counter % delcount === 0) {
+                arr()
+            }
+        }
+        catch (e) {
+            console.log('errorsing')
+            console.log(e)
+            await context.close();
+        }
+    }
+
+    function arr() {
+        webpages.splice(0, delcount).map((webpage, i) => {
+            run(webpage);
+        })
+    }
+    arr()
+
+
+})();
