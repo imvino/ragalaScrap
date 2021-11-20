f0436f4a2c39cfff88fcb7f202c386bcabfee1d5
imvino <49083863+imvino@users.noreply.github.com>
2021-11-20 15:11:48 +0530

    Update transferCat2Gender.js

--- ragalaCms/transferCat2Gender.js
+++ ragalaCms/transferCat2Gender.js
@@ -57,7 +57,7 @@ async function newUrlGen(db) {
     let actress = [16, 20, 22, 23, 24, 29, 32, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 56, 57, 59, 61, 63]
     if (db == 'starzone_photos') {
         let r1 = await databaseCms("SELECT  `rid`,`catgen`,`permaLink`,`gender` FROM `starzone_photos` WHERE `nurl` IS NULL order by `rid` desc");
-        if (r1.length==0) console.log(db+' over')
+        if (r1.length == 0) console.log(db + ' over')
         r1.map((v, i) => {
             let rid = v.rid
             let gallery = urlSlug(v.permaLink)
@@ -81,79 +81,112 @@ async function newUrlGen(db) {
         })
     }
     if (db == 'movies_function' || db == 'movies_photos' || db == 'movies_poster') {
-        let sub = {'movies_function':'functions','movies_photos':'photos','movies_poster':'poster-designs'}
-        let newtbl = db =='movies_photos'? ", `fileLocation` ":''
-        let r1 = await databaseCms("SELECT  `rid`,`permaLink` "+newtbl+" FROM `"+db+"` WHERE `nurl` IS  NULL order by `rid` desc");
-        if (r1.length==0) console.log(db+' over')
-        r1.map((v, i) =>{
+        let sub = {'movies_function': 'functions', 'movies_photos': 'photos', 'movies_poster': 'poster-designs'}
+        let newtbl = db == 'movies_photos' ? ", `fileLocation` " : ''
+        let r1 = await databaseCms("SELECT  `title`,`rid`,`permaLink` " + newtbl + " FROM `" + db + "` WHERE `permaLink` IS  NULL order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
             let rid = v.rid
-            let gallery = urlSlug(v.permaLink)
-            if(db =='movies_photos') {
-              //  console.log(v.fileLocation,v.fileLocation.includes('/includes/working/') )
-                sub[db] = v.fileLocation.includes('/includes/working/') ?  'working-stills' : 'photos'
+            let gallery = v.permaLink ? urlSlug(v.permaLink) : urlSlug(v.title)
+            if (db == 'movies_photos') {
+                //  console.log(v.fileLocation,v.fileLocation.includes('/includes/working/') )
+                sub[db] = v.fileLocation.includes('/includes/working/') ? 'working-stills' : 'photos'
             }
             let url = `/movies/${sub[db]}/${rid}/${gallery}`
             if (url != '') {
-               let up = "UPDATE `"+db+"` SET `nurl` = '" + url + "' where rid=" + rid;
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
                 console.log(up)
                 databaseCms(up)
             }
             if (i == r1.length - 1) console.log('done')
         });
     }
-    if(db=='local_events_events'){
-        let r1 = await databaseCms("SELECT  `rid`,`permaLink`  FROM `"+db+"` WHERE `nurl` IS  NULL and working='200' order by `rid` desc ");
-        if (r1.length==0) console.log(db+' over')
-        r1.map((v, i) =>{
+    if (db == 'local_events_events') {
+        let r1 = await databaseCms("SELECT  `rid`,`permaLink`  FROM `" + db + "` WHERE `nurl` IS  NULL and working='200' order by `rid` desc ");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
             let rid = v.rid
             let gallery = urlSlug(v.permaLink)
             let url = `/events/local-events/${rid}/${gallery}`
             if (url != '') {
-                let up = "UPDATE `"+db+"` SET `nurl` = '" + url + "' where rid=" + rid;
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
                 console.log(up)
                 databaseCms(up)
             }
             if (i == r1.length - 1) console.log('done')
         })
     }
-    if(db=='movies_names_title'){
-        let r1 = await databaseCms("SELECT  `rid`,`web_url`  FROM `"+db+"` WHERE `nurl` IS  NULL and working='200' order by `rid` desc");
-        if (r1.length==0) console.log(db+' over')
-        r1.map((v, i) =>{
+    if (db == 'movies_names_title') {
+        let r1 = await databaseCms("SELECT  `rid`,`permaLink`  FROM `" + db + "` WHERE `nurl` IS NULL and working='200' order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
             let rid = v.rid
-            let url = v.web_url.replace('.aspx','')
+            let gallery = urlSlug(v.permaLink)
+            let url = `/articles/movies/${rid}/${gallery}`
             if (url != '') {
-                let up = "UPDATE `"+db+"` SET `nurl` = '" + url + "' where rid=" + rid;
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
                 console.log(up)
                 databaseCms(up)
             }
             if (i == r1.length - 1) console.log('done')
         })
     }
-    if(db=='starzone_filmpersonal'){
-        let r1 = await databaseCms("SELECT  `rid`,`permaLink`  FROM `"+db+"` WHERE `nurl` IS  NULL and working='200' order by `rid` desc");
-        if (r1.length==0) console.log(db+' over')
-        r1.map((v, i) =>{
+    if (db == 'movies_reviews' || db == 'articles_editorial' || db == 'articles_interviews') {
+        let r1 = await databaseCms("SELECT  `rid`,`permaLink`,`title`  FROM `" + db + "` WHERE `nurl` IS NULL and working='200' order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
+            let rid = v.rid
+            let gallery = v.permaLink ? urlSlug(v.permaLink) : urlSlug(v.title)
+            let url_tbl = db == 'movies_reviews' ? `movie-reviews` : db == 'articles_editorial' ? `editorial` : `interviews`;
+            let url = `/articles/${url_tbl}/${rid}/${gallery}`
+            if (url != '') {
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
+                console.log(up)
+                databaseCms(up)
+            }
+            if (i == r1.length - 1) console.log('done')
+        })
+    }
+    if (db == 'articles_press_releases' || db == 'articles_news') {
+        let r1 = await databaseCms("SELECT  `rid`,`permaLink`,`heading`  FROM `" + db + "` WHERE `nurl` IS NULL and working='200' order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
+            let rid = v.rid
+            let gallery = v.permaLink ? urlSlug(v.permaLink) : urlSlug(v.heading)
+            let url_tbl = db == 'articles_press_releases' ?'press-releases' : 'news'
+            let url = `/articles/${url_tbl}/${rid}/${gallery}`
+            if (url != '') {
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
+                console.log(up)
+                databaseCms(up)
+            }
+            if (i == r1.length - 1) console.log('done')
+        })
+    }
+    if (db == 'starzone_filmpersonal') {
+        let r1 = await databaseCms("SELECT  `rid`,`permaLink`  FROM `" + db + "` WHERE `nurl` IS  NULL and working='200' order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
             let rid = v.rid
             let gallery = urlSlug(v.permaLink)
             let url = `/starzone/info/${rid}/${gallery}`
             if (url != '') {
-                let up = "UPDATE `"+db+"` SET `nurl` = '" + url + "' where rid=" + rid;
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
                 console.log(up)
                 databaseCms(up)
             }
             if (i == r1.length - 1) console.log('done')
         })
     }
-    if(db=='local_events_location'){
-        let r1 = await databaseCms("SELECT  `rid`,`locationName`  FROM `"+db+"` WHERE `nurl` IS  NULL and working='200' order by `rid` desc");
-        if (r1.length==0) console.log(db+' over')
-        r1.map((v, i) =>{
+    if (db == 'local_events_location') {
+        let r1 = await databaseCms("SELECT  `rid`,`locationName`  FROM `" + db + "` WHERE `nurl` IS  NULL and working='200' order by `rid` desc");
+        if (r1.length == 0) console.log(db + ' over')
+        r1.map((v, i) => {
             let rid = v.rid
             let gallery = urlSlug(v.locationName)
             let url = `/events/info/${rid}/${gallery}`
             if (url != '') {
-                let up = "UPDATE `"+db+"` SET `nurl` = '" + url + "' where rid=" + rid;
+                let up = "UPDATE `" + db + "` SET `nurl` = '" + url + "' where rid=" + rid;
                 console.log(up)
                 databaseCms(up)
             }
@@ -162,6 +195,15 @@ async function newUrlGen(db) {
     }
 
 }
+
+
+let db = ['starzone_photos', 'movies_function', 'movies_photos', 'movies_poster', 'local_events_events', 'movies_names_title',
+    'starzone_filmpersonal', 'local_events_location', 'movies_reviews', 'articles_press_releases', 'articles_editorial'
+    ,'articles_interviews','articles_news']
+newUrlGen('articles_news')
+//Ocat2Ncat()
+//transferCat2Gender()
+//newId()
 
 // async function Ocat2Ncat() {
 //     let r1 = await database("SELECT  `rid`,`category` FROM `starzone_filmpersonal` WHERE `category` IS NOT NULL  order by `rid` desc");
@@ -182,9 +224,3 @@ async function newUrlGen(db) {
 //
 //     console.log('total',sql)
 //          }
-
-let db = ['starzone_photos', 'movies_function','movies_photos','movies_poster','local_events_events','movies_names_title','starzone_filmpersonal','local_events_location']
-newUrlGen('local_events_events')
-//Ocat2Ncat()
-//transferCat2Gender()
-//newId()
